0ee74aeea229cc38af1536e27dedc190283f723c
tianchan.ztc <tianchan.ztc@dtwave-inc.com>
2021-07-21 19:30:36 +0800

    chore: test prettier

--- src/pages/main/main.jsx
+++ src/pages/main/main.jsx
@@ -1,9 +1,7 @@
-import React from "react"
+import React from "react";
 
 const Main = () => {
-  return <div>12</div>
-}
+  return <div>12</div>;
+};
 
-
-
-export default Main
+export default Main;
