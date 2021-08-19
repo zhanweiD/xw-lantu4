a071691a65b59a5591c3f856aebaea16e24a8243
tianchan.ztc <tianchan.ztc@dtwave-inc.com>
2021-08-19 16:24:38 +0800

    refactor: 去除不需要的material文件

--- src/views/sidebar/material-panel/material-toolbar.jsx
+++ src/views/sidebar/material-panel/material-toolbar.jsx
@@ -1,16 +1,20 @@
-import React from "react"
+import React, {useState} from "react"
 import {observer} from "mobx-react-lite"
 import {useTranslation} from "react-i18next"
 import c from "classnames"
 import IconButton from "@components/icon-button"
 import w from "@models"
 import s from "./material-panel.module.styl"
+import Modal from "@components/modal"
+import {TextField} from "@components/field"
 
 const MaterialToolbar = () => {
   const {t} = useTranslation()
   const {sidebar} = w
   const {materialPanel} = sidebar
   const {keyword, showType, searchMaterials} = materialPanel
+  const [isVisible, setIsVisible] = useState(false)
+  const [name, setName] = useState("")
   return (
     <div className={c("fbh fbac cfw2 pl8", s.toolbar)}>
       <div className="fb1">
@@ -53,7 +57,8 @@ const MaterialToolbar = () => {
               {
                 name: "新建文件夹",
                 action: () => {
-                  materialPanel.createFolder()
+                  // materialPanel.createFolder()
+                  setIsVisible(true)
                   menu.hide()
                 }
               },
@@ -68,6 +73,42 @@ const MaterialToolbar = () => {
           })
         }}
       />
+      <Modal
+        title="新建文件夹"
+        height={160}
+        width={320}
+        isVisible={isVisible}
+        onClose={() => {
+          setIsVisible(false)
+        }}
+        buttons={[
+          {
+            name: "取消",
+            action: () => {
+              setIsVisible(false)
+              setName("")
+            }
+          },
+          {
+            name: "新增",
+            action: () => {
+              materialPanel.createFolder(name)
+              setIsVisible(false)
+              setName("")
+            }
+          }
+        ]}
+      >
+        <TextField
+          value={name}
+          onChange={(value) => {
+            setName(value)
+          }}
+          className="pt8 pb8"
+          label={t("name")}
+          placeholder="文件夹名称不能为空、重复"
+        />
+      </Modal>
     </div>
   )
 }
