c424b01e4abe889c844396e8da7db499897ecf91
tianchan.ztc <tianchan.ztc@dtwave-inc.com>
2021-08-30 15:51:33 +0800

    refactor: 配置文件模版参考v1.0

--- src/exhibit-collection/mock.js
+++ src/exhibit-collection/mock.js
@@ -1,62 +1,111 @@
-const exhibit = {
-  layers: [
+/* addLayer: () => {
+
+} */
+/* addConfig: () => {
+
+} */
+
+const getLayersConfig = () => {
+  return [
     {
       key: "bubble-layer",
       name: "气泡层",
-      data: {
-        type: "private"
-        // defaultValue: [],
-      },
-      styles: [
+      icons: [
         {
-          label: "label",
-          option: "label",
-          sectionConfig: [
+          icon: "eyes"
+        },
+        {
+          icon: "more"
+          // type: ''
+        }
+      ],
+      configs: [
+        {
+          option: "data",
+          field: {
+            type: "data"
+          }
+        },
+        {
+          label: "位置",
+          option: "position",
+          fields: [
             {
-              option: "isVisible",
-              field: {
-                type: "sectionConfig",
-                icon: "eyes",
-                defaultValue: true
+              type: "data-map",
+              label: "经度",
+              option: "lat",
+              link: (target, self) => {
+                target.event.on("change", (v) => {
+                  self.setOptions(v)
+                })
               }
+            },
+            {
+              type: "data-map",
+              label: "纬度",
+              option: "lan",
+              link: (target, self) => {
+                target.event.on("change", (v) => {
+                  self.setOptions(v)
+                })
+              }
+            }
+          ]
+        },
+        {
+          label: "标签",
+          option: "label",
+          canSetVisible: true,
+          icons: [
+            {
+              icon: "eyes",
+              option: "isVisible"
             }
           ],
           fields: [
             {
+              label: "字段",
+              type: "data-map",
               option: "field",
-              subscribe: {
-                action: (value) => {
-                  console.log(value)
-                }
-              },
-              field: {
-                type: "select"
+              link: (target, self) => {
+                target.event.on("change", (v) => {
+                  self.setOptions(v)
+                })
               }
             },
             {
+              label: "字号",
+              type: "number",
               option: "fontSize",
-              field: {
-                type: "text",
-                step: 1,
-                defaultValue: 10
-              }
+              step: 1,
+              min: 12
             },
             {
-              option: "color",
-              field: {
-                type: "color",
-                link: (config, self) => {
-                  config.reverse.on("change", (v) => {
-                    self.xxxReverseColor(v)
-                  })
+              label: "颜色",
+              type: "color",
+              option: "color"
+            },
+            {
+              label: "透明度",
+              option: "opacity",
+              step: 0.01,
+              min: 0,
+              max: 1,
+              defaultValue: 1
+            },
+            {
+              label: "偏移",
+              option: "offset",
+              type: "multiNumber",
+              defaultValue: [0, 0],
+              items: [
+                {
+                  key: "X"
+                },
+                {
+                  key: "Y"
                 }
-              }
-            },
-            {
-              option: "reverse",
-              field: {
-                type: "switch"
-              }
+              ]
             }
           ]
         }
@@ -64,3 +113,10 @@ const exhibit = {
     }
   ]
 }
+const config = () => ({
+  key: "gis-map",
+  name: k("gis-map"),
+  layout: () => [16, 16],
+  getLayerConfig: () => getLayersConfig(),
+  configs: []
+})
