a080d91bb66083b18a5e18b7591e26e845196c51
拂山 <song.js@dtwave-inc.com>
2021-09-08 14:59:57 +0800

    更新fields

---
+++ src/exhibit-option-system/sections.js
@@ -0,0 +1,20 @@
+/**
+ * =====================================
+ * gis
+ * =====================================
+ */
+
+//  const pointCoordinate = {
+//   name: "pointCoordinate",
+
+//   fields: [
+//     {
+//       name: "lang",
+//       defaultValue: 12
+//     },
+//     {
+//       name: "lat",
+//       defaultValue: 10
+//     }
+//   ]
+// }
