d14e389fd5c2117c0dbd47011f0022945694bf0b
shuzhiwen <zhiwen.szw@dtwave-inc.com>
2021-09-10 12:28:51 +0800

    feat：素材面板优化

--- src/views/sidebar/material-panel/material-panel.jsx
+++ src/views/sidebar/material-panel/material-panel.jsx
@@ -1,32 +1,32 @@
-import React, {useState} from "react"
-import {observer} from "mobx-react-lite"
-import {useTranslation} from "react-i18next"
-import c from "classnames"
-import Tab from "@components/tab"
-import Icon from "@components/icon"
-import Loading from "@components/loading"
-import IconButton from "@components/icon-button"
-import w from "@models"
-import Modal from "@components/modal"
-import Scroll from "@components/scroll"
-import {TextField} from "@components/field"
-import MaterialFolder from "./material-folder"
-import s from "./material-panel.module.styl"
-import MaterialToolbar from "./material-toolbar"
+import React, {useState} from 'react'
+import {observer} from 'mobx-react-lite'
+import {useTranslation} from 'react-i18next'
+import c from 'classnames'
+import Tab from '@components/tab'
+import Icon from '@components/icon'
+import Loading from '@components/loading'
+import IconButton from '@components/icon-button'
+import w from '@models'
+import Modal from '@components/modal'
+import Scroll from '@components/scroll'
+import {TextField} from '@components/field'
+import MaterialFolder from './material-folder'
+import s from './material-panel.module.styl'
+import MaterialToolbar from './material-toolbar'
 
 const MoreIcon = ({materialPanel, folder, isTop}) => {
   const {toggleFolderTop, exportFolder, removeFolder} = materialPanel
-  const menu = w.overlayManager.get("menu")
+  const menu = w.overlayManager.get('menu')
   const onClickMore = (e, button) => {
     e.stopPropagation()
     menu.toggle({
       attachTo: button,
       list: [
-        {name: "上传素材", action: () => (folder.set({isVisible: true}), menu.hide())},
-        {name: `${isTop ? "取消置顶" : "置顶"}文件夹`, action: () => (toggleFolderTop(folder), menu.hide())},
-        {name: "导出文件夹", action: () => (exportFolder(folder), menu.hide())},
-        {name: "删除文件夹", action: () => (removeFolder(folder), menu.hide())}
-      ]
+        {name: '上传素材', action: () => (folder.set({isVisible: true}), menu.hide())},
+        {name: `${isTop ? '取消置顶' : '置顶'}文件夹`, action: () => (toggleFolderTop(folder), menu.hide())},
+        {name: '导出文件夹', action: () => (exportFolder(folder), menu.hide())},
+        {name: '删除文件夹', action: () => (removeFolder(folder), menu.hide())},
+      ],
     })
   }
   return (
@@ -40,7 +40,7 @@ const MoreIcon = ({materialPanel, folder, isTop}) => {
 // 项目面板无项目时的 UI
 const MaterialFallback = ({keyword, set, noProject}) =>
   keyword ? (
-    <div className={c("m8 emptyNote")}>
+    <div className={c('m8 emptyNote')}>
       <div className="fbh fbjc">{`抱歉，没有找到与"${keyword}"相关的素材`}</div>
     </div>
   ) : noProject ? (
@@ -64,7 +64,7 @@ const MaterialFallback = ({keyword, set, noProject}) =>
 
 const MaterialPanel = () => {
   const {t} = useTranslation()
-  const [name, setName] = useState("")
+  const [name, setName] = useState('')
   const {sidebar} = w
   const {materialPanel} = sidebar
   const {set, state, folders_, projectFolders_, projectId, showType, keyword, isVisible, createFolder} = materialPanel
@@ -73,7 +73,7 @@ const MaterialPanel = () => {
   return (
     <Loading data={state}>
       <Tab sessionId="material-panel-tab" bodyClassName="fbv" className="w100p h100p">
-        <Tab.Item name={t("materialPanel.project")}>
+        <Tab.Item name={t('materialPanel.project')}>
           <MaterialToolbar useCreate />
           <Scroll className="h100p">
             {topProjectFolders.map((folder) => (
@@ -97,7 +97,7 @@ const MaterialPanel = () => {
             )}
           </Scroll>
         </Tab.Item>
-        <Tab.Item name={t("materialPanel.space")}>
+        <Tab.Item name={t('materialPanel.space')}>
           <MaterialToolbar useCreate />
           <Scroll className="h100p">
             {topFolders.map((folder) => (
@@ -119,7 +119,7 @@ const MaterialPanel = () => {
             {!basicFolders.length && !topFolders.length && <MaterialFallback keyword={keyword} set={set} />}
           </Scroll>
         </Tab.Item>
-        <Tab.Item name={t("materialPanel.official")}>
+        <Tab.Item name={t('materialPanel.official')}>
           <MaterialToolbar />
           <div className="fbv fbac fbjc mt30 pt30">
             <div className="p10 fbv fbac fs10 lh32">
@@ -136,15 +136,15 @@ const MaterialPanel = () => {
         isVisible={isVisible}
         onClose={() => set({isVisible: false})}
         buttons={[
-          {name: "取消", action: () => (set({isVisible: false}), setName(""))},
-          {name: "新增", action: () => createFolder(name, () => setName(""))}
+          {name: '取消', action: () => (set({isVisible: false}), setName(''))},
+          {name: '新增', action: () => createFolder(name, () => setName(''))},
         ]}
       >
         <TextField
           value={name}
           onChange={(value) => setName(value)}
           className="pt8 pb8"
-          label={t("name")}
+          label={t('name')}
           placeholder="文件夹名称不能为空、重复"
         />
       </Modal>
--- src/views/sidebar/material-panel/material-toolbar.jsx
+++ src/views/sidebar/material-panel/material-toolbar.jsx
@@ -1,34 +1,35 @@
-import React from "react"
-import {observer} from "mobx-react-lite"
-import {useTranslation} from "react-i18next"
-import c from "classnames"
-import IconButton from "@components/icon-button"
-import w from "@models"
-import s from "./material-panel.module.styl"
+import IconButton from '@components/icon-button'
+import w from '@models'
+import c from 'classnames'
+import {observer} from 'mobx-react-lite'
+import React, {useState} from 'react'
+import {useTranslation} from 'react-i18next'
+import s from './material-panel.module.styl'
 
 const MaterialToolbar = ({useCreate}) => {
   const {t} = useTranslation()
   const {sidebar} = w
   const {materialPanel} = sidebar
-  const {keyword, showType, searchFolders, set} = materialPanel
+  const {showType, set} = materialPanel
+  const [keyword, setKeyword] = useState('')
   return (
-    <div className={c("fbh fbac cfw2 pl8", s.toolbar)}>
+    <div className={c('fbh fbac cfw2 pl8', s.toolbar)}>
       <div className="fb1">
         <input
           type="text"
           value={keyword}
-          placeholder={t("searchPlaceholder")}
-          onChange={(e) => set({keyword: e.target.value})}
+          placeholder={t('searchPlaceholder')}
+          onChange={(e) => setKeyword(e.target.value)}
         />
       </div>
-      {keyword && <IconButton icon="close" title={t("remove")} onClick={() => set({keyword: ""})} />}
-      <IconButton icon="search" className="cfw6" title={t("search")} onClick={searchFolders} />
+      {keyword && <IconButton icon="close" title={t('remove')} onClick={() => (setKeyword(''), set({keyword: ''}))} />}
+      <IconButton icon="search" className="cfw6" title={t('search')} onClick={() => set({keyword})} />
       <IconButton icon={showType} className="cfw10" title="显示切换" onClick={materialPanel.toggleShowType} />
       {useCreate && (
         <IconButton
           icon="create-material"
           className="cfw12"
-          title={t("materialPanel.materialCreate")}
+          title={t('materialPanel.materialCreate')}
           onClick={(e) => (e.stopPropagation(), set({isVisible: true}))}
         />
       )}
