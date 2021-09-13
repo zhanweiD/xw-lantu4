2c21a248b5488c5a9d28e1d5e48f3ad62586e7a5
tianchan.ztc <tianchan.ztc@dtwave-inc.com>
2021-09-13 21:23:40 +0800

    fix: 向后兼容，已有的组件新增的section取默认值

--- src/exhibit-collection/create-exhibit-layer-class.js
+++ src/exhibit-collection/create-exhibit-layer-class.js
@@ -20,7 +20,6 @@ const createLayer = (key, layer, env) => {
         // 需要判断是否是gis，如果是gis就把数据塞到每一层里去
         const MConfig = transform({id, sections})
         self.options = MConfig.create()
-        console.log(self.options.getRelationFields('columnSelect'))
         self.data = MDataField.create(
           {
             type: 'data',
