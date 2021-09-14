f1c203a79b6b0aad3fa30f120523dbe440b93a6e
shuzhiwen <zhiwen.szw@dtwave-inc.com>
2021-09-14 10:02:34 +0800

    feat：关闭最后一个editor-tab的时候更新项目素材信息

--- src/models/editor/editor-tab.js
+++ src/models/editor/editor-tab.js
@@ -1,98 +1,97 @@
-import {getEnv, types} from "mobx-state-tree"
-import isFunction from "lodash/isFunction"
-import commonAction from "@utils/common-action"
-import {MArt} from "../art/art"
-import {MArtInit} from "./editor-tab-art-init"
-import {MProjectDetail} from "./editor-tab-project-detail"
-import {MMaterial} from "./editor-tab-material"
-import {MDataTab} from "./editor-tab-data"
-import {MDataSourceManager} from "./editor-tab-data-manager"
+import {getEnv, types} from 'mobx-state-tree'
+import isFunction from 'lodash/isFunction'
+import commonAction from '@utils/common-action'
+import {MArt} from '../art/art'
+import {MArtInit} from './editor-tab-art-init'
+import {MProjectDetail} from './editor-tab-project-detail'
+import {MMaterial} from './editor-tab-material'
+import {MDataTab} from './editor-tab-data'
+import {MDataSourceManager} from './editor-tab-data-manager'
 
 export const MEditorTab = types
   .model({
     id: types.union(types.number, types.string),
-    name: types.optional(types.string, ""),
-    type: types.enumeration(["art", "projectDetail", "artInit", "material", "data", "dataSourceManager"]),
+    name: types.optional(types.string, ''),
+    type: types.enumeration(['art', 'projectDetail', 'artInit', 'material', 'data', 'dataSourceManager']),
     projectDetail: types.maybe(MProjectDetail),
     material: types.maybe(MMaterial),
     data: types.maybe(MDataTab),
     initArt: types.maybe(MArtInit),
     tabOptions: types.frozen(),
     art: types.maybe(MArt),
-    dataSourceManager: types.maybe(MDataSourceManager)
+    dataSourceManager: types.maybe(MDataSourceManager),
   })
   .views((self) => ({
     get env_() {
       return getEnv(self)
-    }
+    },
   }))
-  .actions(commonAction(["set"]))
+  .actions(commonAction(['set']))
   .actions((self) => {
     const showDetail = () => {
       const {event} = self.env_
       const {type, id, tabOptions} = self
-      if (type === "projectDetail") {
+      if (type === 'projectDetail') {
         // ! description 有问题，后续改
         const {name} = self
         self.projectDetail = {
           id,
-          name
+          name,
         }
         self.projectDetail.getDetail()
       }
 
-      if (type === "artInit") {
+      if (type === 'artInit') {
         const {projectId} = tabOptions
         self.initArt = {
-          name: "",
+          name: '',
           id,
-          projectId
+          projectId,
         }
       }
-      if (type === "art") {
+      if (type === 'art') {
         if (!self.art) {
           self.art = {
-            artId: id
+            artId: id,
           }
           self.art.getArt()
         }
       }
-      if (type === "material") {
+      if (type === 'material') {
         if (!self.material) {
           self.material = {
-            materialId: self.id
+            materialId: self.id,
           }
           self.material.getMaterialDetail()
         }
       }
       // 设置项目素材的 projectId
-      // TODO: 非项目素材打开的时候会与当前激活的项目绑定，待优化
-      if (type !== "art" && type !== "material") {
-        event.fire("materialPanel.setProjectId", {projectId: null})
+      if (type !== 'art' && type !== 'material') {
+        event.fire('materialPanel.setProjectId', {projectId: null})
       } else {
         const {projectId} = self.tabOptions
-        event.fire("materialPanel.setProjectId", {
-          projectId
+        event.fire('materialPanel.setProjectId', {
+          projectId,
         })
       }
-      if (type === "data") {
+      if (type === 'data') {
         if (!self.data) {
           const {dataType, folderId, isProject, projectId = 0} = tabOptions
           self.data = {
-            dataId: typeof id === "number" ? id : 0,
+            dataId: typeof id === 'number' ? id : 0,
             dataType,
             folderId,
             isProject,
-            projectId
+            projectId,
           }
           self.data.getData()
         }
       }
-      if (type === "dataSourceManager") {
+      if (type === 'dataSourceManager') {
         if (!self.dataSourceManager) {
           self.dataSourceManager = {
             id,
-            ...tabOptions
+            ...tabOptions,
           }
           self.dataSourceManager.getDatabaseSource()
         }
@@ -101,19 +100,19 @@ export const MEditorTab = types
 
     const save = () => {
       const {type, art, data, material} = self
-      if (type === "art" && art && isFunction(art.save)) {
+      if (type === 'art' && art && isFunction(art.save)) {
         art.save()
       }
-      if (type === "data" && data) {
+      if (type === 'data' && data) {
         data.saveData()
       }
-      if (type === "material" && material) {
+      if (type === 'material' && material) {
         material.save()
       }
     }
 
     return {
       showDetail,
-      save
+      save,
     }
   })
