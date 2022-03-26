ece7593c6cec7bc45a8c00303ee59cc060b868cb
zz.ye <zizhong.yzz@dtwave-inc.com>
2022-03-26 15:26:17 +0800

    feat(add):直方图

---
+++ src/waves4/waves/v3/histogram/index.js
@@ -0,0 +1,17 @@
+import {config} from './config'
+import makeAdapter from './adapter'
+import i18n from './i18n'
+
+const histogram = {
+  lib: 'wave',
+  id: 'NzT7',
+  version: '1.0.0',
+  completed: true,
+  description: '',
+  icon: 'histogram',
+  i18n,
+  config,
+  makeAdapter,
+}
+
+export default histogram
