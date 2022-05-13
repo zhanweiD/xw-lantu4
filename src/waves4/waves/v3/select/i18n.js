c9cab68c898e90807e6760788f06164cf186e980
郑少宁 <zhengshaoning@zhengshaoningdeMacBook-Pro.local>
2022-05-13 18:47:57 +0800

    feat: 添加下拉框组件的样式配置

--- src/waves4/waves/v3/select/i18n.js
+++ src/waves4/waves/v3/select/i18n.js
@@ -3,4 +3,7 @@ export default {
   select: ['下拉框', 'Select'],
   option: ['选项', 'Option'],
   FocusBackgroundColor: ['悬停颜色', 'Focus Background Color'],
+  optionFontColor: ['选项文字颜色', 'Option FontColor'],
+  optionBackgroundColor: ['选项默认背景色', 'Option Default Background'],
+  optionHoverBackgroundColor: ['选项的背景悬浮时色', 'Option Hover Background'],
 }
