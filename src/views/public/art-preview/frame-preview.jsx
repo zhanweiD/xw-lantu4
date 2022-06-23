33dd8537bad91d20de882f6f454078f9b61c7261
zhanwei.dzw <zhanwei.dzw@dtwave-inc.com>
2022-06-23 11:05:31 +0800

    自适应

--- src/views/public/art-preview/frame-preview.jsx
+++ src/views/public/art-preview/frame-preview.jsx
@@ -7,11 +7,12 @@ import Material from '../material'
 import {themeConfigs} from '@common/theme'
 
 const ArtFrame = ({art, frame}) => {
-  const {global, overflowX, overflowY, zoom} = art
+  const {global, overflowX, overflowY, zoom, totalHeight, totalWidth} = art
   const {effective, fields} = global.options.sections.watermark
   const {originLayout, frameId, layout, boxes, materials = [], backgroundImage_, backgroundColor_} = frame
   const {theme} = global.options.sections.themeColor.fields
-
+  const {heightAdaption, widthAdaption} = global.options.sections.screenAdaption.fields
+  const adaptionEff = global.options.sections.screenAdaption.effective
   const reverseMaterials = cloneDeep(materials)
   reverseMaterials.reverse()
   const style = {
@@ -31,18 +32,28 @@ const ArtFrame = ({art, frame}) => {
   if (!backgroundColor_ && !backgroundImage_) {
     style.background = themeConfigs[theme].background
   }
-  useEffect(() => {
-    const resizeFun = () => {
-      art.zoom.init(document.querySelector(`#artFrame-${frameId}`))
+  const scaleArt = () => {
+    const art = document.querySelector(`#artFrame-${frameId}`)
+    const x = window.innerWidth / originLayout.width
+    const y = window.innerHeight / originLayout.height
+    art.style.transform = `scale(${x}, ${y})`
+  }
+  const resizeFun = () => {
+    if (adaptionEff) {
+      if (heightAdaption === 'zoomToScreenHeight' && widthAdaption === 'zoomToScreenWidth') return scaleArt()
+      // else if (heightAdaption === 'scrollVertical' && widthAdaption === 'scrollHorizontal') return null
     }
+    art.zoom.init(document.querySelector(`#artFrame-${frameId}`), {})
+  }
+
+  useEffect(() => {
     window.addEventListener('resize', resizeFun)
     return () => window.removeEventListener('resize', resizeFun)
   }, [frameId])
 
   useEffect(() => {
-    art.zoom.init(document.querySelector(`#artFrame-${frameId}`))
+    resizeFun()
   }, [frameId])
-
   return (
     <div
       id={`artFrame-${frameId}`}
@@ -50,6 +61,7 @@ const ArtFrame = ({art, frame}) => {
       style={{
         overflowX,
         overflowY,
+        transformOrigin: '0% 0%',
         width: originLayout.width,
         height: originLayout.height,
       }}
