025ec8716f92843f81581ee700e7a0321eafd492
zhanwei.dzw <zhanwei.dzw@dtwave-inc.com>
2022-06-28 14:40:54 +0800

    package

--- src/materials/decorations/parser/index.js
+++ src/materials/decorations/parser/index.js
@@ -1,7 +1,14 @@
+/*
+ * @Author: zhanwei
+ * @Date: 2022-06-21 15:51:01
+ * @LastEditors: zhanwei
+ * @LastEditTime: 2022-06-28 14:40:19
+ * @Description:
+ */
 // import {createWave} from '@waveview/wave/src/main'
 // import {createWave} from '@waveview/wave'
-// import {createWave} from 'dtwave-chart'
-import {createWave} from 'dtwave-chart/src/main'
+import {createWave} from 'dtwave-chart'
+// import {createWave} from 'dtwave-chart/src/main'
 import translate from './translate'
 
 export {createWave, translate}
