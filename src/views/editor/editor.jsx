614c4f83af17729003cb2118ccbb1aa14638209e
郑少宁 <zhengshaoning@zhengshaoningdeMacBook-Pro.local>
2022-07-19 09:56:07 +0800

    fix: 修复测试问题

--- src/views/editor/editor.jsx
+++ src/views/editor/editor.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {observer} from 'mobx-react-lite'
 // import {useTranslation} from 'react-i18next'
 import c from 'classnames'
@@ -10,9 +10,10 @@ import EditorTab from './editor-tab'
 import s from './editor.module.styl'
 
 const Editor = () => {
-  const {editor, overlayManager, sidebar, head} = w
-  const {activePanelButton} = head
+  const {editor, overlayManager, sidebar} = w
   const {tabs, activeTabId, updateActiveNote, closeTab, closeAllTabs, closeOtherTabs} = editor
+  const [name, setName] = useState('')
+  const [frameId, setFrameId] = useState(0)
   const {projectPanel} = sidebar
   const {set, isCloseModalVisible} = projectPanel
   // const {t} = useTranslation()
@@ -97,7 +98,9 @@ const Editor = () => {
                 iconSize={12}
                 className={s.tabCloseIcon}
                 onClick={() => {
-                  activePanelButton === 'projects' ? set('isCloseModalVisible', true) : closeTab(tab.id)
+                  tab.id.toString().length > 30 ? closeTab(tab.id) : set('isCloseModalVisible', true)
+                  setName(tab.name)
+                  setFrameId(tab.id)
                 }}
               />
               <Modal
@@ -114,14 +117,14 @@ const Editor = () => {
                   {
                     name: '取消',
                     action: () => {
-                      closeTab(tab.id, 'cancel')
+                      closeTab(frameId, 'cancel')
                       set('isCloseModalVisible', false)
                     },
                   },
                   {
                     name: '确认',
                     action: () => {
-                      closeTab(tab.id, 'confirm')
+                      closeTab(frameId, 'confirm')
                       set('isCloseModalVisible', false)
                     },
                   },
@@ -141,7 +144,7 @@ const Editor = () => {
                     <path d="M512 64C264.6 64 64 264.6 64 512s200.6 448 448 448 448-200.6 448-448S759.4 64 512 64zm0 820c-205.4 0-372-166.6-372-372s166.6-372 372-372 372 166.6 372 372-166.6 372-372 372z"></path>
                     <path d="M464 688a48 48 0 1096 0 48 48 0 10-96 0zm24-112h48c4.4 0 8-3.6 8-8V296c0-4.4-3.6-8-8-8h-48c-4.4 0-8 3.6-8 8v272c0 4.4 3.6 8 8 8z"></path>
                   </svg>
-                  <span>{`是否保存对当前"${tab.name}"的修改？`}</span>
+                  <span>{`是否保存对当前"${name}"的修改？`}</span>
                 </div>
               </Modal>
             </div>
